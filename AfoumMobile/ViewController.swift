b627e7c54453608e4f445fa88df52ccf6b1f51df
Priscilla <prikoo@gmail.com>
2019-03-02 13:25:45 -0500

    Initial Commit

---
+++ AfoumMobile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AfoumMobile
+//
+//  Created by Priscilla Koo on 3/2/19.
+//  Copyright © 2019 Afuom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
